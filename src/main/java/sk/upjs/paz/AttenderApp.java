4cf49bcf3070f80119647c048e2b81ea27ddd042
Matúš Klimko <matklim09@gmail.com>
2025-10-06 12:22:04 +0200

    congratulations

---
+++ src/main/java/sk/upjs/paz/AttenderApp.java
@@ -0,0 +1,32 @@
+package sk.upjs.paz;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+public class AttenderApp extends Application {
+	@Override
+	public void start(Stage stage) throws Exception {
+		var loader = new FXMLLoader(getClass().getResource("attender.fxml"));
+		Parent rootPane = loader.load();
+
+		var scene = new Scene(rootPane);
+		stage.setTitle("Attendances");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	// WARNING: This main is used for running the app from terminal using `mvn javafx:run`.
+	// This main will not work when running from IntelliJ IDEA, hence use IDELauncher.java instead.
+	// Explanation: The issue is the Java 9+ module system that JavaFX requires.
+	// However, modules are a nightmare to get working with maven, hence we do not use them at this moment.
+	// As a dirty but effective hack we can use another main method that calls this one.
+	// Hence, you should rather run IDELauncher.java
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
---
+++ src/main/java/sk/upjs/paz/IDELauncher.java
@@ -0,0 +1,10 @@
+package sk.upjs.paz;
+
+public class IDELauncher {
+
+	// This main method is used for running the app from IntelliJ IDEA.
+	public static void main(String[] args) {
+		AttenderApp.main(args);
+	}
+}
+
